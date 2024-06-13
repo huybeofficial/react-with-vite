0c93f08175b135ea0adae7b629f75a30969986ea
huybe <huybeofficial@gmail.com>
2024-06-13 22:33:16 +0700

    Add CSS Baseline

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,7 @@
-import { useState } from 'react'
-
 function App() {
-const [count, setCount] = useState(0)
-console.log(count)
   return (
     <>
-    Trần Quang Huy
+      <div>Trần Quang Huy</div>
     </>
   )
 }
