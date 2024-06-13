ee697f7d00497b85a696ad65ec8d25d8f9703729
huybe <huybeofficial@gmail.com>
2024-06-14 02:10:10 +0700

    add mui theme default

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,26 @@
+import { Typography, Button } from "@mui/material"
+import { useColorScheme } from '@mui/material/styles'
+
+
+function ModeToggle() {
+  const { mode, setMode } = useColorScheme()
+  return (
+    <Button
+      onClick={() => {
+        setMode(mode === 'light' ? 'dark' : 'light')
+      }}
+    >
+      {mode === 'light' ? 'Turn dark' : 'Turn light'}
+    </Button>
+  )
+}
+
+
 function App() {
   return (
     <>
-      <div>Trần Quang Huy</div>
+      <ModeToggle />
+      <Button variant="contained" color="secondary">Contained</Button>
     </>
   )
 }
